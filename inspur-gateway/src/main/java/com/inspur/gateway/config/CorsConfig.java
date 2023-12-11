0a4ab36f28f2eefa92306fa185f6f2101e7a4ebb
siooo <1453352875@qq.com>
2023-12-11 11:05:43 +0800

    fix bug1

--- inspur-gateway/src/main/java/com/inspur/gateway/config/CorsConfig.java
+++ inspur-gateway/src/main/java/com/inspur/gateway/config/CorsConfig.java
@@ -3,21 +3,47 @@ package com.inspur.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 import org.springframework.web.util.pattern.PathPatternParser;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
-    public CorsWebFilter corsWebFilter(){
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedMethod("*");
-        config.addAllowedOrigin("http://106.14.137.153:81");
-        config.addAllowedHeader("*");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
-        source.registerCorsConfiguration("/**", config);
-        return new CorsWebFilter(source);
+    public WebFilter corsFilter2() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                HttpHeaders requestHeaders = request.getHeaders();
+                ServerHttpResponse response = ctx.getResponse();
+                HttpMethod requestMethod = requestHeaders.getAccessControlRequestMethod();
+                HttpHeaders headers = response.getHeaders();
+                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://106.14.137.153:81");
+                headers.addAll(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+                        requestHeaders.getAccessControlRequestHeaders());
+                if (requestMethod != null) {
+                    headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, requestMethod.name());
+                }
+                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+                headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(ctx);
+        };
     }
 }
